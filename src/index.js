e47cb24dd64b3596a7a1a84c3864799efbec693a
RMalone8 <rmalone@bu.edu>
2026-02-11 15:06:00 -0500

    removed d1 usage, added flight plan handling for backend

--- src/index.js
+++ src/index.js
@@ -1,52 +1,72 @@
-import { mosaicUpload, mosaicRetrieval } from "./storage";
+import storage from "./storage";
 
 export default {
     async fetch(request, env) {
         const url = new URL(request.url);
 
+        const corsHeaders = {
+            "Access-Control-Allow-Origin": "http://localhost:5173",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        };
+
+        if (request.method === "OPTIONS") {
+            return new Response(null, {
+                status: 204,
+                headers: corsHeaders
+            });
+        }
+
         // Basic Security Check
-        if (request.headers.get("Authorization") !== `Bearer ${env.DEVICE_TOKEN}`) {
-            console.log(env.DEVICE_TOKEN);
-            return new Response("Unauthorized", { status: 401 });
+        if (request.headers.get("Authorization") !== `Bearer ${env.DEVICE_TOKEN}`) {;
+            console.log(request.headers.get("Authorization"));
+            return new Response("Unauthorized", { status: 401, headers: corsHeaders });
         }
 
-        // 1. WAYPOINTS (Web App POSTs them, Pi GETs them)
-        if (url.pathname === "/waypoints") {
+        // FLIGHT PLAN (Web App POSTs them, Pi GETs them)
+        if (url.pathname === "/flightplan") {
             if (request.method === "POST") {
-                const waypoints = await request.json();
-                return new Response("Waypoints Saved");
+                const flightplan = await request.json();
+                
+                console.log(flightplan);
+                await storage.flightPlanUpload(env, "1001", flightplan);
+                return new Response("Flight Plan Saved", {headers: {
+                    ...corsHeaders,
+                    "Content-Type": "application/json"
+                }});
             }
-            return new Response(waypoints || "[]");
+            const flightplan = await storage.flightPlanRetrieval(env, "1001");
+            return Response.json(flightplan || "[]", {headers: corsHeaders });
         }
 
-        // 2. TELEMETRY (Pi POSTs status, Web App GETs status)
+        // TELEMETRY (Pi POSTs status, Web App GETs status)
         if (url.pathname === "/telemetry") {
             if (request.method === "POST") {
                 const telemetry = await request.json();
-                return new Response("Status Updated");
+                return new Response("Status Updated", {headers: corsHeaders });
             }
-            return new Response(telemetry || "{}");
+            return new Response(telemetry || "{}", {headers: corsHeaders });
         }
 
-        // 3. MOSAICS (Pi POSTs data, Web App GETs data)
+        // MOSAICS (Pi POSTs data, Web App GETs data)
         if (url.pathname === "/mosaic") {
             if (request.method === "POST") {
                 const data = await request.json();
                 try {
-                    await mosaicUpload(env, "1001", JSON.stringify(data));
+                    await storage.mosaicUpload(env, "1001", JSON.stringify(data));
                     console.log("Complete!");
                 } catch (e) {
                     console.error("Mosaic Error: ", e.message);
                 }
                 
-                return new Response("Status Updated");
+                return new Response("Status Updated", {headers: corsHeaders });
             }
-            const data = await mosaicRetrieval(env, "1001");
+            const data = await storage.mosaicRetrieval(env, "1001");
             const text = await data.text();
-            return new Response(text || "{}");
+            return new Response(text || "{}", {headers: corsHeaders });
         }
 
 
-        return new Response("Not Found", { status: 404 });
+        return new Response("Not Found", { status: 404, headers: corsHeaders });
     }
 };
--- src/storage.js
+++ src/storage.js
@@ -1,16 +1,41 @@
-export async function mosaicUpload(env, user_id, content) {
-    const path = `data/${user_id}/mosaic/mosaic1.txt`;
+export default {
 
-    const object = await env.BUCKET.put(path, content, {
-        httpMetadata: { contentType: "application/json"}
-    });
+    // mosaic functions
+    async mosaicRetrieval(env, user_id) {
+        const path = `data/${user_id}/mosaic/mosaic1.txt`;
+    
+        return await env.BUCKET.get(path);
+    },
 
-    console.log(`Successfully uploaded ${object.key} to R2`);
-    return object;
-}
+    async mosaicUpload(env, user_id, content) {
+        const path = `data/${user_id}/mosaic/mosaic1.txt`;
+    
+        const object = await env.BUCKET.put(path, content, {
+            httpMetadata: { contentType: "application/json"}
+        });
+    
+        console.log(`Successfully uploaded ${object.key} to R2`);
+        return object;
+    },
 
-export async function mosaicRetrieval(env, user_id) {
-    const path = `data/${user_id}/mosaic/mosaic1.txt`;
+    //flight plan functions
+    async flightPlanUpload(env, user_id, content) {
+        const path = `data/${user_id}/fp/fp1.json`;
+    
+        const object = await env.BUCKET.put(path, JSON.stringify(content), {
+            httpMetadata: { contentType: "application/json"}
+        });
+    
+        console.log(`Successfully uploaded ${object.key} to R2`);
+        return object;
+    },
 
-    return await env.BUCKET.get(path);
+    async flightPlanRetrieval(env, user_id) {
+        const path = `data/${user_id}/fp/fp1.json`;
+    
+        const data = await env.BUCKET.get(path);
+
+        return await data.json();
+    }
+
 }
